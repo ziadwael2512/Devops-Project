1d398ca4dfb58e10575e9ce29e9518e96b741fb7
ziadwael2512 <146972516+ziadwael2512@users.noreply.github.com>
2025-07-12 17:16:07 +0300

    Update waitdb.sh

--- backend/waitdb.sh
+++ backend/waitdb.sh
@@ -2,9 +2,9 @@
 
 # Wait until Postgres is ready
 until pg_isready -h db -p 5432 -U postgres; do
-  echo "⏳ Waiting for PostgreSQL..."
+  echo " Waiting for PostgreSQL..."
   sleep 2
 done
 
-echo "✅ PostgreSQL is ready. Starting backend..."
+echo "PostgreSQL is ready. Starting backend..."
 exec node index.js
